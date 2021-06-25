bf98512ae0a81b04efd6294da258299a6e9d946e
GlebKlimakhin <71899242+GlebKlimakhin@users.noreply.github.com>
2021-06-25 21:39:11 +0300

    Add files via upload

---
+++ src/main/java/com/gb/spring/les3/repositories/Repository.java
@@ -0,0 +1,10 @@
+package com.gb.spring.les3.repositories;
+
+import com.gb.spring.les3.models.Product;
+
+import java.util.List;
+
+public interface Repository {
+
+    public List<Product> get();
+}
